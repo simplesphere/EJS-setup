c22e4051c0187cd3a9f45432199114942110a0f7
simplesphere <anoop.jeewoolall@gmail.com>
2016-01-28 15:34:24 -0500

    updated server.js to run express

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 /// <reference path="_reference.ts" />
-var http = require('http');
-var port = process.env.port || 3000;
-var server = http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end("Hello Node");
+/*
+import http = require('http');
+
+var port:number = process.env.port || 3000;
+
+var server = http.createServer(
+    function(req:http.ServerRequest, res:http.ServerResponse){
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end("Hello Node");
 });
-server.listen(port, function () {
+
+server.listen(port, function(){
     console.log("Server Started.. Listening on port: " + port);
+});*/
+var express = require('express');
+var app = express();
+var port = process.env.port || 3000;
+// Main route
+app.get('/', function (req, res, next) {
+    res.send('Hello World!');
+});
+// info route
+app.get('/info', function (req, res) {
+    res.send('Info Page');
+});
+app.listen(port, function () {
+    console.log('Example app listening on port', +port);
 });
 //# sourceMappingURL=server.js.map
--- server.ts
+++ server.ts
@@ -1,5 +1,6 @@
 /// <reference path="_reference.ts" />
 
+/*
 import http = require('http');
 
 var port:number = process.env.port || 3000;
@@ -12,4 +13,26 @@ var server = http.createServer(
 
 server.listen(port, function(){
     console.log("Server Started.. Listening on port: " + port);
+});*/
+
+import express = require('express');
+
+var app:express.Express = express();
+
+var port:number = process.env.port || 3000;
+
+// Main route
+app.get('/', 
+    function (req:express.Request, res:express.Response, next:any) {
+    res.send('Hello World!');
+});
+
+// info route
+app.get('/info', 
+    function (req:express.Request, res:express.Response) {
+    res.send('Info Page');
+});
+
+app.listen(port, function () {
+  console.log('Example app listening on port', + port);
 });
